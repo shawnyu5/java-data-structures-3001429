48ae7b973436e5d9ac37cd42bae38d040cc60020
Shawn Yu <shawn.yu5@hotmail.com>
2022-01-16 18:10:42 -0500

    queues understood

--- src/main/java/QueueExamples.java
+++ src/main/java/QueueExamples.java
@@ -5,17 +5,15 @@ import java.util.Queue;
 
 public class QueueExamples {
 
-  public static void main(String[] args) {
+   public static void main(String[] args) {
 
-    Queue<String> queue = new ArrayDeque<>();
-    queue.offer("Person One");
-    queue.offer("Person Two");
-    queue.offer("Person Three");
-    System.out.println(queue);
-    System.out.println(queue.peek());
-    queue.poll();
-    System.out.println(queue);
-    
-  }
-
+      Queue<String> queue = new ArrayDeque<>();
+      queue.offer("Person One");
+      queue.offer("Person Two");
+      queue.offer("Person Three");
+      System.out.println(queue);
+      // System.out.println(queue.peek());
+      queue.poll();
+      System.out.println(queue);
+   }
 }
